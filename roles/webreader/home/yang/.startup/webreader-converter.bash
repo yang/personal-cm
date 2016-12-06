e15f60a7a5b510550d2914028aaf69e2fdd801bf
Yang Zhang <yaaang@gmail.com>
2016-12-05 18:27:53 -0800

    Use TMPDIR for web-reader

--- roles/webreader/home/yang/.startup/webreader-converter.bash
+++ roles/webreader/home/yang/.startup/webreader-converter.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 { cat ~/.screenrc; echo 'logfile "/tmp/webreader-converter.log"'; echo 'deflog on' ; } > /tmp/webreader-converter.screenrc
-screen -c /tmp/webreader-converter.screenrc -D -m -U -S webreader-converter bash -lc 'workon web-reader; web-reader converter -t yaaang@gmail.com'
+screen -c /tmp/webreader-converter.screenrc -D -m -U -S webreader-converter bash -lc 'workon web-reader; TMPDIR=/mnt/store/yang/tmp/ web-reader converter -t yaaang@gmail.com'
